1f05c9e4f8d6313b1ff24d03eeaca8d2dba9113b
Vulp0 <mauriciomartinezvaz@gmail.com>
2023-04-15 15:11:09 -0600

    kinda works i guess

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+let pswrd;
 let prepositionList;
 let adjectiveList;
 let nounList;
 let verbList;
 let symbolList;
+let questionList;
 let numberLength = 6;
 
 let randomWord = (wordArray) => wordArray[Math.floor(Math.random() * wordArray.length)];
@@ -38,32 +40,36 @@ fetch("./import/symbols.json")
         symbolList = data;
     });
 
+function showMe(newText){
+    document.querySelector(".results").innerHTML = newText;
+}
 
 function getPass(){
     switch(Math.floor(Math.random() * 6)){
         case 0:
-            console.log("0");
-            console.log(randomWord(adjectiveList) + randomWord(nounList) + randomWord(prepositionList) + randomWord(nounList) + randomWord(symbolList) + randomNumber());
+            // console.log("0");
+            pswrd = randomWord(adjectiveList) + randomWord(nounList) + randomWord(prepositionList) + randomWord(nounList) + randomWord(symbolList) + randomNumber();
+            showMe(pswrd);
             break;
         case 1:
-            console.log("1");
-            console.log(randomWord(verbList) + randomWord(nounList) + randomNumber());
+            // console.log("1");
+            pswrd = randomWord(verbList) + randomWord(nounList) + randomNumber();
+            showMe(pswrd);
             break;
         case 2:
-            console.log("2");
-            console.log();
+            // console.log("2");
+            pswrd = "i" + randomWord(verbList) + randomWord(nounList) + randomWord(symbolList) + randomWord(nounList) + randomNumber();
+            showMe(pswrd);
             break;
         case 3:
-            console.log("3");
-            console.log();
+            // console.log("3");
+            pswrd = randomWord(adjectiveList) + randomWord(nounList) + randomNumber();
+            showMe(pswrd);
             break;
         case 4:
-            console.log("4");
-            console.log();
-            break;
-        case 5:
-            console.log("5");
-            console.log();
+            // console.log("4");
+            pswrd = randomWord(verbList) + "the" + randomWord(nounList) + randomWord(prepositionList) + randomWord(symbolList) + randomWord(nounList) + randomNumber();
+            showMe(pswrd);
             break;
     }
 }
